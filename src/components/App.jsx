d2142d3362f424a734709e90fced32c563e48e83
kimuniiii <kk.baseball.0824@gmail.com>
2020-10-10 19:54:55 +0900

    Props
    ・Propsの基礎
    ・Appコンポーネントをmap関数を用いてリファクタ

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
 
-const App = () => {
+// const App = (props) => {
+//   return (
+//     <>
+//       <User name={'KKK'} age={10} />
+//       <User name={'KK'} age={5} />
+//     </>
+//   )
+// }
+
+/**
+ * Appコンポーネント
+ * 上でコメントアウトしたコンポーネントのリファクタリング
+ * @param {Object} props
+ * @return {Array}
+ */
+
+const App = (props) => {
+
+  const profiles = [{name:'KKK',age:42},{name:'KK',age:42},{name:'K'}];
+
   return (
     <>
-      <h4>React Redux</h4>
+      {profiles.map((cur,idx) => { return <User key={idx} name={cur.name} age={cur.age} /> })}
     </>
   )
+}
+
+/**
+ * Userコンポーネント
+ * Appコンポーネントの子供のコンポーネント
+ * @param {Object} props
+ * @return JSX
+ */
+
+const User = (props) => {
+  return (
+  <>
+  <h4>{props.name}</h4>
+  <h4>{props.age}</h4>
+  </>
+  )
+}
+
+User.defaultProps = {
+  age: 1,
 }
 
 export default App;
